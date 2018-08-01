521c3d898b28b17c9063714dede70678f5c23d84
belgren <belgren28@gmail.com>
2018-07-31 20:06:33 -0700

    token changes

--- app.js
+++ app.js
@@ -35,13 +35,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 ///////body////////
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", '*');
-    res.header("Access-Control-Allow-Credentials", true);
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
-    res.header("Access-Control-Allow-Headers", 'Origin,X-Requested-With,Content-Type,Accept,content-type,application/json');
-    next();
-});
+
 app.get('/', function(req, res){
   res.render('')
 })
